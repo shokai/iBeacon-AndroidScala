b5b86bafbc46d629ffc221c6d2af264814c37ca3
Sho Hashimoto <hashimoto@shokai.org>
2014-05-16 17:31:53 +0900

    generate project template

---
+++ gen/R.java.d
@@ -0,0 +1,8 @@
+/Users/sho/src/android/scala/IBeaconTestApp/gen/org/shokai/ibeacontestapp/R.java \
+ : /Users/sho/src/android/scala/IBeaconTestApp/res/drawable-hdpi/ic_launcher.png \
+/Users/sho/src/android/scala/IBeaconTestApp/res/drawable-ldpi/ic_launcher.png \
+/Users/sho/src/android/scala/IBeaconTestApp/res/drawable-mdpi/ic_launcher.png \
+/Users/sho/src/android/scala/IBeaconTestApp/res/drawable-xhdpi/ic_launcher.png \
+/Users/sho/src/android/scala/IBeaconTestApp/res/layout/main.xml \
+/Users/sho/src/android/scala/IBeaconTestApp/res/values/strings.xml \
+/Users/sho/src/android/scala/IBeaconTestApp/bin/AndroidManifest.xml \
---
+++ gen/org/shokai/ibeacontestapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.shokai.ibeacontestapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
