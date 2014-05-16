11ba0710f6965e3b2e7a3f8c64c83c1ef835cc63
Sho Hashimoto <hashimoto@shokai.org>
2014-05-16 18:13:50 +0900

    fix View

--- src/org/shokai/ibeacontestapp/MainActivity.scala
+++ src/org/shokai/ibeacontestapp/MainActivity.scala
@@ -2,10 +2,24 @@ package org.shokai.ibeacontestapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.{TextView, EditText, Button};
+import android.view.View;
+import android.view.View.OnClickListener;
 
 class MainActivity extends Activity{
+
+  lazy val textViewMsg:TextView = findViewById(R.id.textViewMsg).asInstanceOf[TextView]
+
   override def onCreate(savedInstanceState:Bundle){
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+
+    trace("app start")
+    textViewMsg.setText("はい")
+  }
+
+  def trace(msg:String){
+    Log.v("iBeaconTestApp", msg)
   }
 }
