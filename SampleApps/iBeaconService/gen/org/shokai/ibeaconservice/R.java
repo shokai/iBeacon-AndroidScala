1302011d7c23d2de7292673ccb68f50c6d7d6519
Sho Hashimoto <hashimoto@shokai.org>
2014-05-19 18:35:12 +0900

    add "stop service" button  #1

--- SampleApps/iBeaconService/gen/org/shokai/ibeaconservice/R.java
+++ SampleApps/iBeaconService/gen/org/shokai/ibeaconservice/R.java
@@ -15,6 +15,7 @@ public final class R {
     }
     public static final class id {
         public static final int btnStart=0x7f050000;
+        public static final int btnStop=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
