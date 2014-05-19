c05db74de96e03078e669954b0ce35445ff5e7da
Sho Hashimoto <hashimoto@shokai.org>
2014-05-19 16:11:55 +0900

    backto MainActivity on tap Notification  #1

--- SampleApps/iBeaconService/src/org/shokai/ibeaconservice/Notifer.scala
+++ SampleApps/iBeaconService/src/org/shokai/ibeaconservice/Notifer.scala
@@ -5,12 +5,14 @@ import android.content.{Context, Intent};
 
 class Notifer(appName:String, context:Context){
 
+  lazy val manager:NotificationManager =
+    context.getSystemService(Context.NOTIFICATION_SERVICE).asInstanceOf[NotificationManager]
+
   def popup(msg:String):Notifer = {
-    val manager:NotificationManager =
-      context.getSystemService(Context.NOTIFICATION_SERVICE).asInstanceOf[NotificationManager]
     val notif:Notification =
       new Notification(android.R.drawable.btn_default, msg, System.currentTimeMillis())
-    val intent:Intent = new Intent(context, context.getClass())
+
+    val intent:Intent = new Intent(context, classOf[MainActivity])
     val contentIntent:PendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
 
     notif.setLatestEventInfo(context.getApplicationContext(), appName, msg, contentIntent)
