ad6937d978645b0e7dfc51f46a7577de46cdd3a6
Sho Hashimoto <hashimoto@shokai.org>
2014-05-19 14:20:41 +0900

    IntentService -> Service  #1

--- SampleApps/iBeaconService/src/org/shokai/ibeaconservice/IBeaconService.scala
+++ SampleApps/iBeaconService/src/org/shokai/ibeaconservice/IBeaconService.scala
@@ -6,24 +6,42 @@ import android.os.{Bundle, IBinder, Looper, Handler, HandlerThread, Process, Mes
 import android.content.Intent;
 import android.util.Log;
 
-class IBeaconService extends IntentService("IBeaconService"){
+class IBeaconService extends Service{
 
   lazy val appName:String = getResources().getString(R.string.app_name)
   lazy val iBeacon:IBeacon = new IBeacon(this)
   lazy val notifer:Notifer = new Notifer(appName, this)
 
-  override def onHandleIntent(intent:Intent){
-    trace("service start")
+  var handler:Handler = null
 
-    iBeacon.onDetect((beacon:Beacon) =>
-      trace(s"UUID=${beacon.uuid} Major=${beacon.major} Minor=${beacon.minor} RSSI=${beacon.rssi}")
-    )
+  override def onCreate{
+    trace("onCreate")
 
+    var thread:HandlerThread = new HandlerThread(appName, Process.THREAD_PRIORITY_BACKGROUND)
+    thread.start()
+    val looper = thread.getLooper()
+    handler = new Handler(looper){
+      override def handleMessage(msg:Message){
+        Log.v("iBeaconService", "handleMessage")
+
+        iBeacon.onDetect((beacon:Beacon) =>
+          trace(s"UUID=${beacon.uuid} Major=${beacon.major} Minor=${beacon.minor} RSSI=${beacon.rssi}")
+        )
+
+      }
+    }
   }
 
   override def onStartCommand(intent:Intent, flags:Int, startId:Int):Int = {
     trace("startCommand")
-    return super.onStartCommand(intent, flags, startId)
+    var msg:Message = handler.obtainMessage()
+    msg.arg1 = startId
+    handler.sendMessage(msg)
+    return Service.START_STICKY
+  }
+
+  override def onBind(intent:Intent):IBinder = {
+    return null
   }
 
   def trace(msg:String){
